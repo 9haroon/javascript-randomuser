4cf11de05269764339c2de20b275cecb74966894
Natto <70090705+zx4545zx@users.noreply.github.com>
2022-03-27 17:34:36 +0700

    Able show data and sort data

--- user-random/src/App.jsx
+++ user-random/src/App.jsx
@@ -1,45 +1,66 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css";
+
+const App = () => {
+  const url = "https://randomuser.me/api/?results=5";
+  const [user, setUser] = useState([]);
+  const [sort, setSort] = useState(false);
+
+  useEffect(() => {
+    fetch(url, {
+      method: "GET",
+      contentType: "application/json",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // console.table(data.results);
+        setUser(data.results);
+      });
+  }, []);
+
+  const sortMax = (data) => {
+    setSort(false);
+    setUser(data.slice().sort((a, b) => b.dob.age - a.dob.age));
+  };
+
+  const sortMin = (data) => {
+    setSort(true);
+    setUser(data.slice().sort((a, b) => a.dob.age - b.dob.age));
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+      <h1>Random Users</h1>
 
-export default App
+      {/* User List */}
+      <div className="header">
+        <section>Full Name</section>
+        <section>
+          <div>Age</div>
+          {sort === true ? (
+            <div onClick={() => sortMax(user)}>&#8651;</div>
+          ) : (
+            <div onClick={() => sortMin(user)}>&#8652;</div>
+          )}
+        </section>
+        <section>Gender</section>
+        <section>Email</section>
+        <section>Tel</section>
+      </div>
+      {user.map((user) => {
+        return (
+          <div key={user.email} className="content">
+            <section>{`${user.name.first} ${user.name.last}`}</section>
+            <section>{user.dob.age}</section>
+            <section>{user.gender}</section>
+            <section>{user.email}</section>
+            <section>{user.phone}</section>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
