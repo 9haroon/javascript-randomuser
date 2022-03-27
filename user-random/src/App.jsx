9091356697a9420ec2908168b116ae45c9b0ffcd
Natto <70090705+zx4545zx@users.noreply.github.com>
2022-03-28 01:12:31 +0700

    Refactor code and file

--- user-random/src/App.jsx
+++ user-random/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState, useEffect } from "react";
 
-import UserList from "./UserList";
+import UserList from "./component/UserList";
 import "./App.css";
 
 const App = () => {
   const url = "https://randomuser.me/api/?results=50";
   const [user, setUser] = useState([]);
   const [userAge, setUserAge] = useState("");
-  const [sort, setSort] = useState(false);
 
   useEffect(() => {
     fetch(url, {
@@ -15,49 +14,27 @@ const App = () => {
       contentType: "application/json",
     })
       .then((response) => response.json())
-      .then((data) => {
-        setUser(data.results);
-      });
+      .then((data) => setUser(data.results));
   }, []);
-
-  const sortMax = (data) => {
-    setSort(false);
-    setUser(data.slice().sort((a, b) => b.dob.age - a.dob.age));
-  };
-
-  const sortMin = (data) => {
-    setSort(true);
-    setUser(data.slice().sort((a, b) => a.dob.age - b.dob.age));
-  };
 
   return (
     <div className="App">
       <h1>Users</h1>
-      <input type="number" onChange={(e) => setUserAge(e.target.value)} />
+      <input type="number" placeholder="age" onChange={(e) => setUserAge(e.target.value)} />
 
       <div className="header">
         <section>Full Name</section>
-        <section>
-          <div>Age</div>
-          {sort === true ? (
-            <div onClick={() => sortMax(user)}>&#8651;</div>
-          ) : (
-            <div onClick={() => sortMin(user)}>&#8652;</div>
-          )}
-        </section>
+        <section>Age</section>
         <section>Gender</section>
         <section>Email</section>
         <section>Tel</section>
       </div>
 
-      {user
-        .filter((u) => u.dob.age == userAge)
-        .map((user) => (
-          <UserList key={user.email} user={user} />
-        ))}
-
-      {userAge == "" &&
-        user.map((user) => <UserList key={user.email} user={user} />)}
+      {userAge == ""
+        ? user.map((user) => <UserList key={user.email} user={user} />)
+        : user
+            .filter((u) => u.dob.age == userAge)
+            .map((user) => <UserList key={user.email} user={user} />)}
     </div>
   );
 };
